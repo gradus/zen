43aa59116ca3c69f1fbb25ce543cf0ae9a41625c
gradus <kriswindham@gmail.com>
2012-05-05 19:21:44 -0400

    zen commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+#Zen is great in any language
+
+### elementary C code to display zen in programming
+ 
+Code and poetry can both be very zen.
+When first learning Python, the first thing I did was type `import this`
+I thought the poem or serious of statements by Tim Peters was great.
+I still see a lot of truth and beauty in these statements.
+I look at code as art.
+
+### Usage
+
+    git clone https://github.com/gradus/zen.git
+    cd zen
+    sudo cp zen /usr/local/bin
+    
+Now, you can just simply type `zen` from your console anytime you need a reminder of the these truths.
---
+++ zen.c
@@ -0,0 +1,32 @@
+/***********************************************************/
+/* The Zen of Python in C, by Tim Peters                   */
+/***********************************************************/
+
+#include <stdio.h>
+
+/***********************************************************/
+
+   main ()             /* Zen */
+
+   {
+   printf ("The Zen of Python, by Tim Peters \n\n");
+   printf ("Beautiful is better than ugly. \n");
+   printf ("Explicit is better than implicit. \n");
+   printf ("Simple is better than complex. \n");
+   printf ("Complex is better than complicated. \n");
+   printf ("Flat is better than nested. \n");
+   printf ("Sparse is better than dense. \n");
+   printf ("Readability counts. \n");
+   printf ("Special cases aren't special enough to break the rules. \n");
+   printf ("Although practicality beats purity. \n");
+   printf ("Errors should never pass silently. \n");
+   printf ("Unless explicitly silenced. \n");
+   printf ("In the face of ambiguity, refuse the temptation to guess. \n");
+   printf ("There should be one-- and preferably only one --obvious way to do it. \n");
+   printf ("Although that way may not be obvious at first unless you're Dutch. \n");
+   printf ("Now is better than never. \n");
+   printf ("Although never is often better than *right* now. \n");
+   printf ("If the implementation is hard to explain, it's a bad idea. \n");
+   printf ("If the implementation is easy to explain, it may be a good idea. \n");
+   printf ("Namespaces are one honking great idea -- let's do more of those! \n");
+   }
